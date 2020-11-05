72b9ae2274d33c74fd295545800bb4001ae82043
Bolbi <bolbi.ket@gmail.com>
2020-11-05 14:13:48 +0100

    WIP navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Composants/home/home.component';
+import { SignInComponent } from './Composants/sign-in/sign-in.component';
+import { SignUpComponent } from './Composants/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/first', pathMatch: 'full' },
+  { path: 'accueil', component:  HomeComponent},
+  { path: 'rejoindre', component:  SignUpComponent},
+  { path: 'connexion', component: SignInComponent},
+];
+
+export const appRouting = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Composants/home/home.component';
+import { TopbarComponent } from './Composants/topbar/topbar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import { SignUpComponent } from './Composants/sign-up/sign-up.component';
+import { SignInComponent } from './Composants/sign-in/sign-in.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopbarComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
