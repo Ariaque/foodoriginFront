5af793542208a778a61e223a89a4f5480d55133c
Emile <emile.georget@etuidiant.univ-rennes1.fr>
2020-11-12 10:47:19 +0100

    add main auth elements

--- src/app/_components/join/join.component.ts
+++ src/app/_components/join/join.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TypeTransformateurService } from '../../_services/type-transformateur.service'
+import { TypeTransformateurService } from '../../_services/type-transformateur.service';
+import { AuthService } from '../../_services/auth.service';
+import {TypeTransformateur} from '../../_classes/type-transformateur';
 
 @Component({
   selector: 'app-join',
@@ -7,14 +9,34 @@ import { TypeTransformateurService } from '../../_services/type-transformateur.s
   styleUrls: ['./join.component.css']
 })
 export class JoinComponent implements OnInit {
-  type  : any;
-  constructor(private data:TypeTransformateurService,) { }
+
+  form: any = {};
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+  siret = this.form.siret;
+  type: TypeTransformateur[];
+  constructor(private authService: AuthService, private data: TypeTransformateurService) { }
 
   ngOnInit(): void {
-    this.data.findAll().subscribe((result)=>{
+    this.data.findAll().subscribe((result) => {
       console.warn(result);
       this.type = result;
-    })
+    });
+  }
+
+  onSubmit(): void {
+    this.authService.register(this.form, this.siret).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
 
 }
