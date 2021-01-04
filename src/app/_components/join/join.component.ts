5079f38dd2ae4577dcbe770328f1005bd2e5b34b
jahouiHajar <jahoui.hajar@gmail.com>
2021-01-04 17:02:34 +0100

    Reactive Form

--- src/app/_components/join/join.component.ts
+++ src/app/_components/join/join.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {TypeTransformateurService} from '../../_services/type-transformateur.service';
 import {AuthService} from '../../_services/auth.service';
 import {TypeTransformateurs} from '../../_classes/type-transformateurs';
 import {TopbarService} from '../../_services/topbar.service';
+import {CustomValidationService} from '../../_services/custom-validation.service';
+import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-join',
@@ -19,13 +21,23 @@ export class JoinComponent implements OnInit {
   siret: number;
   confPassword: string;
   selectedType: TypeTransformateurs;
+  @Input()
+  myForm: FormGroup
 
-  constructor(private authService: AuthService, private typeTransformateurService: TypeTransformateurService, public topBarService: TopbarService) {
+  constructor(private _fb: FormBuilder,private customValidator: CustomValidationService, private authService: AuthService, private typeTransformateurService: TypeTransformateurService, public topBarService: TopbarService) {
   }
 
   ngOnInit(): void {
     this.typeTransformateurService.findAll().subscribe((result) => {
       this.type = result;
+    });
+    this.myForm = this._fb.group({
+      username: [null, [Validators.required, Validators.email]],
+      siret: [null, [Validators.required, Validators.pattern('^[0-9]{14}$')]],
+      password: [null, Validators.required],
+      confPassword: [null, Validators.required],
+    }, {
+      validator: this.customValidator.passwordMatchValidator("password","confPassword")
     });
   }
 
@@ -44,6 +56,22 @@ export class JoinComponent implements OnInit {
         this.isSignUpFailed = true;
       }
     );
+  }
+
+  get numSiret(){
+    const  temp = <FormGroup>this.myForm.controls.siret;
+    return temp;
+  }
+
+  get username() {
+    return this.myForm.get('username');
+  }
+
+  get password() {
+    return this.myForm.get('password');
+  }
+  get confiPassword() {
+    return this.myForm.get('confPassword');
   }
 
 }
