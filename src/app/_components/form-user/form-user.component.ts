2306d28d3b15aa25f0415e2ff3055bd46c75428d
Equilat <emile.georget@outlook.fr>
2021-01-13 10:59:29 +0100

    fix main Facebook and Instagram links were reversed

--- src/app/_components/form-user/form-user.component.ts
+++ src/app/_components/form-user/form-user.component.ts
@@ -124,9 +124,9 @@ export class FormUserComponent implements OnInit {
     this.createUrlList (this.urlVideoForm.value.urls);
     this.createDenreeList (this.denreeForm.value.denrees);
     this.infos = new InfosTransformateur(this.transformateur, this.description, this.nbEmployes, this.lienSite,
-       this.lienInsta, this.lienTwitter, this.lienFacebook, this.appartientGroupe, this.siretGroupe, this.listLabel.value,
+       this.lienFacebook, this.lienTwitter, this.lienInsta, this.appartientGroupe, this.siretGroupe, this.listLabel.value,
        this.listCertif.value, this.urlVideos, this.fermesP, this.denreesA);
-       this.step = 1;
+    this.step = 1;
     this.infosTService.saveInfosTransformateur(this.idInfo, this.infos).subscribe(
       res => {
         alert ('Informations sauvegardées');
