5d272448700ab571fd24a52b2a6cf246f398d786
jahouiHajar <jahoui.hajar@gmail.com>
2021-01-19 10:04:06 +0100

    Syntaxe email

--- src/app/_components/forgot-password/forgot-password.component.ts
+++ src/app/_components/forgot-password/forgot-password.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 import {SendResetPassordEmailService} from '../../_services/send-reset-passord-email.service';
 import {Router} from '@angular/router';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../_services/user.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {User} from '../../_classes/user';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -15,23 +19,40 @@ export class ForgotPasswordComponent implements OnInit {
   email: string;
   @Input()
   myForm: FormGroup;
+  usersSource: MatTableDataSource<User>;
+  data: any;
+  usersname: string[] = [];
 
-  constructor(private _fb: FormBuilder,private router: Router, private sendResetPassordEmailService: SendResetPassordEmailService) { }
+
+  constructor(private userService: UserService, private _fb: FormBuilder,private router: Router, private sendResetPassordEmailService: SendResetPassordEmailService) { }
 
   ngOnInit(): void {
     this.myForm = this._fb.group({
       mail: [null, [Validators.required, Validators.email]]
     });
+    this.userService.findUsers().subscribe(data => {
+      this.usersSource = new MatTableDataSource<User>(data);
+      this.data = data[0].getUsername;
+      for (let i = 0; i < this.usersSource.data.length; i++) {
+        this.usersname.push(data[i].getUsername) ;
+      }
+    });   
+    
   }
 
   onSubmit(): void {
-    this.sendResetPassordEmailService.sendEmail(this.email).subscribe(success => {
-      if (success){
+    if(this.usersname.indexOf(this.email) !== -1){
+      this.sendResetPassordEmailService.sendEmail(this.email).subscribe(success => {
+        if (success){
         this.router.navigate(['/success'], { queryParams: { title: 'Vérifiez vos mails !', text: 'Vérifiez vos mails (et vos spams !) un mail pour réinitialiser votre mot de passe vous a été envoyé !' } });
-      }});
+        }});
+    } else {
+      Swal.fire({title: "Votre mail n'existe pas!"});
+      }
   }
 
   get mail(): AbstractControl {
     return this.myForm.get('mail');
   }
+ 
 }
