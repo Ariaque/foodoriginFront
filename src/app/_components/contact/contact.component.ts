391c93c3d59e4abc0f34488b49b366bb96788484
Yaelle <dubois.e1600202@eutd.univ-ubs.fr>
2021-01-21 16:32:53 +0100

    Resolve merge conflict

--- src/app/_components/contact/contact.component.ts
+++ src/app/_components/contact/contact.component.ts
@@ -35,7 +35,6 @@ export class ContactComponent implements OnInit {
       description: [null, Validators.required]
     });
     if (!this.areShown) {
-      console.log(this.tokenService.getUser());
       this.form.username = this.tokenService.getUser().username;
       this.myForm.controls.username.disable();
       this.myForm.controls.phone.disable();
